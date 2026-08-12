56c050bf2680a89ffdd6c6603d9f83da9730b92b
fw7th <okosa7th@gmail.com>
2026-08-12 13:09:32 +0100

    refactor(tensor.cpp): remove create function

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,13 +1,1 @@
 #include "tensor.h"
-
-void Tensor::create(int _size, Allocator *_allocator) {
-  if (w == _size && allocator == _allocator)
-    return;
-
-  // release();
-
-  allocator = _allocator;
-  w = _size;
-
-  dataptr = allocator->allocate(_size);
-}
